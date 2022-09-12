2792b09c4bf0ff24c9f3a60e3ef145341249ec27
Ronaldo <ronaldovctor@outlook.com>
2022-09-12 19:12:22 -0300

    Starting

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Hello React Native!</Text>
+			<Button title='Click Here' />
+			<StatusBar style='auto' />
+		</View>
+	)
+}
+
+type ButtonProps = {
+	title: string
+}
+
+function Button({ title }: ButtonProps) {
+	return (
+		<TouchableOpacity>
+			<Text>{title}</Text>
+		</TouchableOpacity>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
