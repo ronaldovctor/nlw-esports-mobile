8bf4dfcd9733df3eefc70b39f31e41e4a438026d
Ronaldo <ronaldovctor@outlook.com>
2022-09-13 12:31:12 -0300

    Adding splash scree, home page and games list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'react-native'
 import { Background } from './src/components/Background'
+import {
+	useFonts,
+	Montserrat_400Regular,
+	Montserrat_600SemiBold,
+	Montserrat_700Bold,
+	Montserrat_900Black,
+} from '@expo-google-fonts/montserrat'
+import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Montserrat_400Regular,
+		Montserrat_600SemiBold,
+		Montserrat_700Bold,
+		Montserrat_900Black,
+	})
+
 	return (
 		<Background>
 			<StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+			{fontsLoaded ? <Home /> : <Loading />}
 		</Background>
 	)
 }
