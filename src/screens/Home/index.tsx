798d269c13c44051d6cdfa0ff6b3c54befc8bf2d
Ronaldo <ronaldovctor@outlook.com>
2022-09-15 15:39:16 -0300

    Getting data from api

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,10 +2,19 @@ import { View, Image, FlatList } from 'react-native'
 import { styles } from './styles'
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading'
-import { GameCard } from '../../components/GameCard'
+import { GameCard, GameCardProps } from '../../components/GameCard'
 import { GAMES } from '../../utils/games'
+import { useEffect, useState } from 'react'
 
 export function Home() {
+	const [games, setGames] = useState<GameCardProps[]>([])
+
+	useEffect(() => {
+		fetch('http://192.168.0.104:3333/games')
+			.then((response) => response.json())
+			.then((json) => setGames(json))
+	}, [])
+
 	return (
 		<View style={styles.container}>
 			<Image source={logoImg} style={styles.logo} />
@@ -16,7 +25,7 @@ export function Home() {
 
 			<FlatList
 				contentContainerStyle={styles.contentList}
-				data={GAMES}
+				data={games}
 				keyExtractor={(item) => item.id}
 				renderItem={({ item }) => <GameCard data={item} />}
 				showsHorizontalScrollIndicator={false}
